d01c6f007feb0bed9922ab30a530763ddc7f2694
tinnuon <tinnuon@github.com>
2018-01-18 10:56:52 +0000

    Added on() and off() wrappers

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,30 @@
 #include <mbed.h>
 
+DigitalOut red(PTB22);
+DigitalOut blue(PTB21);
+DigitalOut green(PTE26);
+
+void on()
+{
+  red.write(1);
+  blue.write(1);
+  green.write(1);
+}
+
+void off()
+{
+  red.write(0);
+  blue.write(0);
+  green.write(0);
+}
+
 int main() {
 
-    DigitalOut red(PTB22);
-
     while(1) {
-      red.write(0);
+      off();
       wait(0.5);
-      red.write(1);
+
+      on();
       wait(0.5);
     }
-}
+  }
